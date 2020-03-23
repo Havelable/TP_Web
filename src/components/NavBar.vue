cfd6d8c7d0c04df8c1fea8a7d5e739da0ad4ebe1
Havelable <julien.havel@gmail.com>
2020-03-23 14:24:56 +0100

    Navbar Changes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,10 +2,19 @@
   <div class="nav-bar">
     <router-link class="nav-button" :to="{ name: 'Accueil'}"><h3>Accueil</h3></router-link>
     <router-link class="nav-button" :to="{ name: 'Formulaire'}"><h3>Formulaire</h3></router-link>
-    <router-link class="nav-button" :to="{ name: ''}">
-      <h3>Liste</h3>
-      <img class="icon" src="@/assets/arrow.svg" alt="">
-    </router-link>
+    
+    <div class="nav-drop">
+      <router-link class="nav-button" :to="{ name: 'Liste'}">
+        <h3>Liste</h3><img class="icon" src="@/assets/arrow.svg" alt="">
+      </router-link>
+      <div class="dropdown-content">
+        <a class="nav-button" href="#">Link 1</a>
+        <a class="nav-button" href="#">Link 2</a>
+        <a class="nav-button" href="#">Link 3</a>
+      </div>
+    </div>
+
+    
     <router-link class="nav-button" :to="{ name: 'About'}"><h3>About</h3></router-link>
     <div></div>
   </div>
@@ -29,6 +38,7 @@ export default {
     align-items: center;
   }
 
+
   a, a:hover, a:visited{
     color: inherit;
     text-decoration: inherit;
@@ -39,9 +49,13 @@ export default {
   }
 
   .nav-button{
+    display: flex;
     height: 100%;
     width: 100%;
     text-decoration: none;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
   }
 
   .nav-button:hover{
@@ -58,5 +72,23 @@ export default {
     margin-left: 10px;
     width: 20px;
     height: 20px;
+  }
+
+  .nav-drop{
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+  
+  .dropdown-content{
+    display: none;
+  }
+  .dropdown-content .nav-button{
+    height: 50px;
+  }
+
+  .nav-drop:hover .dropdown-content{
+    display: flex;
+    flex-direction: column;
   }
 </style>
